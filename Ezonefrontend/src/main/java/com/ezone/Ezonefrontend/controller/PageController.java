b8b6bcbb3431793ddb37b0d8a5c2e1768b7613bd
deepak5s <import.deepak@protonmail.com>
2017-12-22 16:24:24 +0530

    Spring Issue Solved

--- Ezonefrontend/src/main/java/com/ezone/Ezonefrontend/controller/PageController.java
+++ Ezonefrontend/src/main/java/com/ezone/Ezonefrontend/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.ezone.Ezonefrontend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,4 +16,29 @@ public class PageController {
 		return mv;
 
 	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView index(@RequestParam(value="greeting",required=false)String greeting) {
+//
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView index(@PathVariable(value="greeting",required=false)String greeting) {
+   if(greeting==null){
+	   greeting="hello";
+   }
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+
+	}
+	
+	
+	
+	
+	
 }
