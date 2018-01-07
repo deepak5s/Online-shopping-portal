8282ab6360ac221aa8588068f6ddcd5f828ce73c
deepak5s <import.deepak@protonmail.com>
2018-01-07 12:20:56 +0530

    Sucessfull Crud opertaion with product testcase

--- Ezonebackend/src/main/java/com/ezone/Ezonebackend/dto/Product.java
+++ Ezonebackend/src/main/java/com/ezone/Ezonebackend/dto/Product.java
@@ -3,10 +3,11 @@ package com.ezone.Ezonebackend.dto;
 import java.util.UUID;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class Product {
 	// private fields are here
 	@Id
